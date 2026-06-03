dc9e436f3f41e7a433f66abcdc2c6a2ec169a8cc
Gabriel L <angelisgabrielluisdeangel@gmail.com>
2026-06-03 13:08:52 -0300

    Conexão Backend e Frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,39 +1,38 @@
-import express from "express";
-import { prisma } from "./config/prisma";
+import express from "express"
+import cors from "cors"
+import { prisma } from "./config/prisma"
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }))
+app.use(express.json())
 
 app.get("/aeronaves", async (req, res) => {
-    const aeronaves = await prisma.aeronave.findMany();
-
-    res.json(aeronaves);
-});
+    const aeronaves = await prisma.aeronave.findMany({
+        include: {
+            pecas: true,
+            etapas: true,
+            testes: true
+        }
+    })
+    res.json(aeronaves)
+})
 
 app.post("/aeronaves", async (req, res) => {
+    const { codigo, modelo, tipo, capacidade, alcance } = req.body
 
-    const {
-        codigo,
-        modelo,
-        tipo,
-        capacidade,
-        alcance
-    } = req.body;
+    const existe = await prisma.aeronave.findUnique({ where: { codigo } })
+    if (existe) {
+        return res.status(409).json({ erro: "Já existe uma aeronave com esse código." })
+    }
 
     const aeronave = await prisma.aeronave.create({
-        data: {
-            codigo,
-            modelo,
-            tipo,
-            capacidade,
-            alcance
-        }
-    });
+        data: { codigo, modelo, tipo, capacidade, alcance }
+    })
 
-    res.status(201).json(aeronave);
-});
+    res.status(201).json(aeronave)
+})
 
 app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000");
-});
+    console.log("Servidor rodando na porta 3000")
+})
